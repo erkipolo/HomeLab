8c48cf06cc4038fb3f59d04f0eb69fad69342b0f
Erki Polo Manresa <erkipolo@gmail.com>
2026-02-04 09:22:16 -0500

    add config fiile
    for root user with public key

--- Terraform/homelab.tf
+++ Terraform/homelab.tf
@@ -1,8 +1,8 @@
-resource "proxmox_lxc" "cts" {
+resource "proxmox_lxc" "srv01" {
   cores           = 1
-  hostname        = "cts"
+  hostname        = "srv01"
   memory          = 512
-  nameserver      = "192.168.56.1 8.8.8.8"
+  nameserver      = "192.168.56.1"
   onboot          = true
   ostemplate      = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
   ssh_public_keys = var.ct_ssh_public_key
@@ -18,71 +18,7 @@ resource "proxmox_lxc" "cts" {
   network {
     bridge   = "vmbr0"
     firewall = true
-    ip       = "192.168.56.8/24"
-    name     = "eth1"
-  }
-  network {
-    bridge   = "vmbr1"
-    firewall = true
-    ip       = "dhcp"
-    name     = "eth0"
-  }
-  rootfs {
-    size    = "100G"
-    storage = "local-lvm"
-  }
-}
-
-resource "proxmox_lxc" "server" {
-  cores           = 1
-  hostname        = "server"
-  memory          = 512
-  nameserver      = "192.168.56.1"
-  onboot          = true
-  ostemplate      = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
-  ssh_public_keys = var.ct_ssh_public_key
-  searchdomain    = "homelab.cu"
-  start           = true
-  swap            = 512
-  target_node     = "pve"
-  unprivileged    = true
-  vmid            = 701
-  features {
-    nesting = true
-  }
-  network {
-    bridge   = "vmbr0"
-    firewall = true
-    ip       = "192.168.56.5/24"
-    name     = "eth0"
-  }
-  rootfs {
-    size    = "100G"
-    storage = "local-lvm"
-  }
-}
-
-resource "proxmox_lxc" "client" {
-  cores           = 1
-  hostname        = "client"
-  memory          = 512
-  nameserver      = "192.168.56.1"
-  onboot          = true
-  ostemplate      = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
-  ssh_public_keys = var.ct_ssh_public_key
-  searchdomain    = "homelab.cu"
-  start           = true
-  swap            = 512
-  target_node     = "pve"
-  unprivileged    = true
-  vmid            = 702
-  features {
-    nesting = true
-  }
-  network {
-    bridge   = "vmbr0"
-    firewall = true
-    ip       = "192.168.56.6/24"
+    ip       = "192.168.56.2/24"
     name     = "eth0"
   }
   rootfs {
