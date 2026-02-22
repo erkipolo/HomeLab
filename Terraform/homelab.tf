46ca019ca6a3eaf785407b58f9b6d02ffccad37d
Erki Polo Manresa <erkipolo@gmail.com>
2026-02-22 11:06:53 -0500

    add some security
    add admin user to pve
    setting timezone
    add kernel security
    add HIPS
    setting HIPS to check SSH service
    add email notification for hips support
    add monitor for check resources locally
    setting monitor to check resources
    add email notification for local monitoring
    add email notification for update unattended
    add snmp for monitoring remotly
    add mod-security on apache
    add mod-evasive on apache
    add and fix some php configuration
    add a role for nextcloud
    add document editting support to nextcloud
    add a role for wordpress (in progress: download, database and web site)
    add a role for zabbix  (in progress: need to fix download, database and web site)
    define ct00 and ct01 for testing
    define ct02 for nextcloud (need more resources than test)

--- Terraform/homelab.tf
+++ Terraform/homelab.tf
@@ -1,14 +1,14 @@
 resource "proxmox_lxc" "ct00" {
-  cores           = 2
+  cores           = 1
   hostname        = "ct00"
-  memory          = 2048
-  nameserver      = "8.8.8.8"
+  memory          = 512
+  nameserver      = "127.0.0.1"
   onboot          = true
   ostemplate      = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
   ssh_public_keys = var.ct_ssh_public_key
   searchdomain    = "homelab.cu"
   start           = true
-  swap            = 2048
+  swap            = 512
   target_node     = "pve"
   unprivileged    = true
   vmid            = 700
@@ -16,34 +16,28 @@ resource "proxmox_lxc" "ct00" {
     nesting = true
   }
   network {
-    bridge   = "vmbr1"
-    firewall = true
-    ip       = "dhcp"
-    name     = "eth0"
-  }
-  network {
     bridge   = "vmbr0"
     firewall = true
     ip       = "192.168.56.10/24"
-    name     = "eth1"
+    name     = "eth0"
   }
   rootfs {
-    size    = "100G"
+    size    = "32G"
     storage = "local-lvm"
   }
 }
 
 resource "proxmox_lxc" "ct01" {
-  cores           = 2
+  cores           = 1
   hostname        = "ct01"
-  memory          = 2048
-  nameserver      = "8.8.8.8"
+  memory          = 512
+  nameserver      = "127.0.0.1"
   onboot          = true
   ostemplate      = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
   ssh_public_keys = var.ct_ssh_public_key
   searchdomain    = "homelab.cu"
   start           = true
-  swap            = 2048
+  swap            = 512
   target_node     = "pve"
   unprivileged    = true
   vmid            = 701
@@ -63,7 +57,36 @@ resource "proxmox_lxc" "ct01" {
     name     = "eth1"
   }
   rootfs {
-    size    = "100G"
+    size    = "32G"
+    storage = "local-lvm"
+  }
+}
+
+resource "proxmox_lxc" "ct02" {
+  cores           = 2
+  hostname        = "ct02"
+  memory          = 2048
+  nameserver      = "127.0.0.1"
+  onboot          = true
+  ostemplate      = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
+  ssh_public_keys = var.ct_ssh_public_key
+  searchdomain    = "homelab.cu"
+  start           = true
+  swap            = 2048
+  target_node     = "pve"
+  unprivileged    = true
+  vmid            = 702
+  features {
+    nesting = true
+  }
+  network {
+    bridge   = "vmbr0"
+    firewall = true
+    ip       = "192.168.56.12/24"
+    name     = "eth0"
+  }
+  rootfs {
+    size    = "32G"
     storage = "local-lvm"
   }
 }
