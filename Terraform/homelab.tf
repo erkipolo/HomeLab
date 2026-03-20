02cc7a2a343d21963cd0c149cea2f0d948fec825
Erki Polo Manresa <erkipolo@gmail.com>
2026-03-20 01:44:18 -0400

    change repos self hosted

--- Terraform/homelab.tf
+++ Terraform/homelab.tf
@@ -1,6 +1,8 @@
 resource "proxmox_lxc" "ct00" {
   cores           = 1
-  hostname        = "ct00"
+  cpulimit        = 0
+  cpuunits        = 100
+  hostname        = "repos"
   memory          = 512
   nameserver      = "127.0.0.1"
   onboot          = true
@@ -11,7 +13,7 @@ resource "proxmox_lxc" "ct00" {
   swap            = 512
   target_node     = "GDMB"
   unprivileged    = true
-  vmid            = 700
+  vmid            = 500
   features {
     nesting = true
   }
@@ -22,12 +24,43 @@ resource "proxmox_lxc" "ct00" {
     name     = "eth0"
   }
   rootfs {
-    size    = "32G"
+    size    = "250G"
     storage = "local-dir"
   }
 }
 
 resource "proxmox_lxc" "ct01" {
+  cores           = 1
+  cpulimit        = 0
+  cpuunits        = 100
+  hostname        = "repo-proxmox"
+  memory          = 512
+  nameserver      = "127.0.0.1"
+  onboot          = true
+  ostemplate      = "local-dir:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
+  ssh_public_keys = var.ct_ssh_public_key
+  searchdomain    = "homelab.cu"
+  start           = true
+  swap            = 512
+  target_node     = "GDMB"
+  unprivileged    = true
+  vmid            = 501
+  features {
+    nesting = true
+  }
+  network {
+    bridge   = "vmbr0"
+    firewall = true
+    ip       = "172.32.40.51/26"
+    name     = "eth0"
+  }
+  rootfs {
+    size    = "40G"
+    storage = "local-dir"
+  }
+}
+
+resource "proxmox_lxc" "ct02" {
   cores           = 1
   hostname        = "ct01"
   memory          = 512
@@ -38,7 +71,7 @@ resource "proxmox_lxc" "ct01" {
   searchdomain    = "homelab.cu"
   start           = true
   swap            = 512
-  target_node     = "pve"
+  target_node     = "GDMB"
   unprivileged    = true
   vmid            = 701
   features {
@@ -62,7 +95,7 @@ resource "proxmox_lxc" "ct01" {
   }
 }
 
-resource "proxmox_lxc" "ct02" {
+resource "proxmox_lxc" "ct03" {
   cores           = 2
   hostname        = "ct02"
   memory          = 2048
@@ -73,7 +106,7 @@ resource "proxmox_lxc" "ct02" {
   searchdomain    = "homelab.cu"
   start           = true
   swap            = 2048
-  target_node     = "pve"
+  target_node     = "GDMB"
   unprivileged    = true
   vmid            = 702
   features {
